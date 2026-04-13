ef51c1cb7fae9f4d5b9a45c787c07ff547b40c38
Nurgül Yalman <123768429+nurshahs@users.noreply.github.com>
2026-04-13 20:24:21 +0300

    7. ders notları bean IoC

--- spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
@@ -1,16 +1,31 @@
 package com.turkcell.spring_starter.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/product")
+import com.turkcell.spring_starter.dto.ProductCreatedResponse;
+import com.turkcell.spring_starter.dto.ProductForCreateDto;
+import com.turkcell.spring_starter.service.ProductServiceImpl;
 
+import jakarta.validation.Valid;
+
+// Altın kural: Veritabanı nesneleri requestte de responseda da kullanılamaz.
+@RestController // Uygulamada gerektiğinde controlleri newle.
+@RequestMapping("/api/product") 
 public class ProductController {
 
-    @GetMapping("")
-    public String sayHi() {
-        return "HELLO";
+
+    //private final ProductServiceImpl productServiceImpl = new ProductServiceImpl();
+    private final ProductServiceImpl productServiceImpl;
+
+    public ProductController(ProductServiceImpl productServiceImpl) {
+        this.productServiceImpl = productServiceImpl;
+    }
+
+    @PostMapping
+    public ProductCreatedResponse create(@RequestBody @Valid ProductForCreateDto productDto) {
+        return this.productServiceImpl.create(productDto);
     }
 }
