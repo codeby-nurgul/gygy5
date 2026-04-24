e0b50353b5fc82e379a5d2801bb0a4c7c012cb10
Nurgül Yalman <123768429+nurshahs@users.noreply.github.com>
2026-04-24 19:24:48 +0300

    add category

---
+++ spring-starter/src/main/java/com/turkcell/spring_starter/entity/Product.java
@@ -0,0 +1,68 @@
+package com.turkcell.spring_starter.entity;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="products")
+public class Product {
+   // @GeneratedValue(strategy = GenerationType.IDENTITY) -> 1'er 1'er artan strateji.
+   @Id
+   @UuidGenerator()
+   @Column(name="id")
+   private UUID id;
+
+   @Column(name="name", nullable = false, length = 100)
+   private String name;
+
+   @Column(name="description", length = 500)
+   private String description;
+
+   @ManyToOne
+   @JoinColumn(name="category_id", nullable = false)
+   private Category category;
+
+
+
+
+   public UUID getId() {
+    return id;
+   }
+
+   public void setId(UUID id) {
+    this.id = id;
+   }
+
+   public String getName() {
+    return name;
+   }
+
+   public void setName(String name) {
+    this.name = name;
+   }
+
+   public String getDescription() {
+    return description;
+   }
+
+   public void setDescription(String description) {
+    this.description = description;
+   }
+
+   public Category getCategory() {
+    return category;
+   }
+
+   public void setCategory(Category category) {
+    this.category = category;
+   }
+
+}
